2e1b2e394eccf0c18414d3a323e3312a1d50e9f0
haqsnich <haqsnich4@gmail.com>
2026-08-26 12:48:28 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,32 @@
+/*
+========================================================
+ระบบประเมินผลงาน
+มหกรรมคุณภาพ ส.เด็กฯ
+ครั้งที่ 28 ประจำปี 2569
+
+config.js
+Version : 2.0.1
+========================================================
+*/
+
+const APP_CONFIG = {
+
+  API_URL:
+    "https://script.google.com/macros/s/AKfycbxNpdxNxFBrvRKRYa_Dm6N62cpGId1vf8c9LfskPWQeeznVmV66w1M55aZ5JBbiwWXCdA/exec",
+
+  APP_NAME:
+    "ระบบประเมินผลงาน",
+
+  EVENT_NAME:
+    "มหกรรมคุณภาพ ส.เด็กฯ",
+
+  EVENT_NUMBER:
+    "ครั้งที่ 28 ประจำปี 2569",
+
+  YEAR:
+    "2569",
+
+  VERSION:
+    "2.0.1"
+
+};
